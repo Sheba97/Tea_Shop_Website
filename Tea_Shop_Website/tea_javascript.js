6015623b97f7a646f7a4c255d6b35e41263d565a
Shehan Ranawaka <66054435+Sheba97@users.noreply.github.com>
2022-04-21 23:38:58 +0530

    Add the all file of the tea shop website

---
+++ Tea_Shop_Website/tea_javascript.js
@@ -0,0 +1,41 @@
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+    cartItem.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+let cartItem = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    cartItem.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
+    
+}
+
+window.onscroll = () => {
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+$(document).ready(function(){
+    $("button").click(function(){
+        $.ajax('learn_more.html',{
+            success:function(result){
+                $('#learn_more_content').html(result.innerHTML);
+            }
+        });
+    });
+});
